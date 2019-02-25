ded0de8d7ed83cbb94e35ba73a313a72b8db139f
khaleesi <missmwtse@hotmail.co.uk>
2019-02-25 12:45:54 +0000

    initial commit

---
+++ dandasite.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+<title>D&A Artspace</title>
+</head>
+
+<body>
+    <header>
+        <img src="img/hero.jpg" alt="artspaceartwork">
+        <nav>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Academy</a></li>
+            <li><a href="">Business</a></li>
+        </nav>
+        <h1>D&A Art Space</h1>
+            <ul>
+                <li><em>Create</em></li>
+                <li><em>Inspire</em></li>
+                <li><em>Educate</em></li>
+            </ul>
+    </header>
+    <main>
+        <section class="about">
+            <h1>The Best Project You Will Ever Work On Is you</h1>
+            <img src="img/about.jpg" alt="Angelo Vallillo">
+            <p>Award Winning Hair Professionals, Dale Herne and Angelo Vallillo, are the latest innovators in the industry. The pair have taught their high in demand courses all over the world including Europe and Asia. Their beliefs are, in order to be the best, it is a must you are up to date with the latest hair techniques.
+    Whether you are an established self employed stylist or a budding entrepreneur D&A can assist you with first-class training for you and your staff. If your desire is to be your own boss, as established business owners D&A can guide you. 
+    All training will take place in D&A Artspace, a Beautiful creative hub in the heart of Nottingham City Centre.</p>
+            <div class="about-rectangle"></div>
+        </section>
+        <section class="academy">
+            <h1>The Academy.</h1>
+            <p>You will be taught by industry professionals Angelo and Dale. All training carried out will be in small groups.</p>
+            <p>“Angelo is a <em>Passionate</em> and <em>Motivating</em> educator, fresh with ideas and <em>Enthusiasm</em>. His unparalleled technical skill is married with a down-to-earth approach that ensures his seminars are <em>Inspirational</em> and <em>Enlightening</em>, yet participants never feel out of their depth. ”</p>
+            <img src="img/academy-01.jpg" alt="Eastern British Hairdressing Awards">
+            <img src="img/academy-02.jpg" alt="Eastern British Hairdressing Awards">
+            <img src="img/academy-03.jpg" alt="Eastern British Hairdressing Awards">
+            <button type="academy-button"><img src="img/right-arrow.svg"></button>
+        </section>
+        <section class="business">
+            <img src="img/business.jpg" alt="Stylist at D&A">
+            <h1>Lets Talk Business.</h1>
+            <p>As well as being inspiring Educators. The pair are reputable business men. Winners of Creative Head Business Thiker of the year. They can give you the right advice to venture into starting up your own hair salon. Click the link above to find out more.</p>
+        </section>
+        <section class="awards">
+            <h1>Awards</h1>
+            <ul>
+                <li><img src="img/bha.png" alt="HJA British hairdressing awards"></li>
+                <li><img src="img/hair.png" alt="Hair awards"></li>
+                <li><img src="img/bha-round.jpg" alt="British hairdressing awards"></li>
+                <li><img src="img/wella.png" alt="Wella trend vision"></li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <section class="contact">
+            <h2>Contact Us.</h2>
+            <a href="tel:0115998298">0115998298</a>
+            <a href="mailto:dna@thednaway.co.uk">Email Us.</a>
+        </section>
+        <section class="socialmedia">
+            <ul>
+            <li><img src="img/googleplus.png" alt="Google Plus"></li>
+            <li><img src="img/twitter.png" alt="Twitter"></li>
+            <li><img src="img/facebook.png" alt="Facebook"></li>
+            <li><img src="img/instagram.png" alt="Instagram"></li>
+            <li><img src="img/youtube.png" alt="Youtube"></li>
+            </ul>
+        </section>
+        <aside class="volcano">
+            <img src="img/volcano_logo.svg" alt="Volcano logo"><p>Site by <a href="http://wearevolcano.com/">Volcano</a></p>   
+        </aside>
+        <aside class="dandalogo">
+            <img src="img/danda.jpg" alt="dandalogo">
+        </aside>
+    </footer>
+</body>
+
+</html>
